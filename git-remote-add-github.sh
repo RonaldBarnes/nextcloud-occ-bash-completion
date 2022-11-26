0094935bef9438a3f242e9a941b4744d04409f62
Ronald Barnes <ron@ronaldbarnes.ca>
2022-11-26 02:36:17 -0800

    Updated URL / name of github repo.

--- git-remote-add-github.sh
+++ git-remote-add-github.sh
@@ -1,3 +1,3 @@
-git remote add github git@github.com:RonaldBarnes/nextcloud-occ.git
-git remote add w00 10.60.42.69/nextcloud-occ-bash-completion.git
+git remote add github git@github.com:RonaldBarnes/nextcloud-occ-bash-completion.git
+## git remote add w00 10.60.42.69/nextcloud-occ-bash-completion.git
 git remote -v
